cfb4dfd0efbc52836e25e53ddd8b360eb2c75159
elisabethribeiro <nutrielisabeth.ribeiro@gmail.com>
2025-09-25 20:44:32 -0300

    atualizado1

---
+++ aula-03/aula-03.ts
@@ -0,0 +1,61 @@
+// Descrição: Limpeza e Resumo de Dados de Pesquisas
+// Você trabalha como analista de dados em uma empresa de tecnologia. O time de pesquisa enviou uma planilha com respostas de usuários sobre um novo aplicativo.
+// Cada registro de resposta tem:
+// id: número identificador da resposta;
+// usuario: nome do usuário que respondeu;
+// notaSatisfacao: número de 1 a 5;
+// duplicado: boolean que indica se a resposta é duplicada.
+// comentario: string com o comentário do usuário.
+
+interface RespostaPesquisa {
+  id: number;
+  usuario: string;
+  notaSatisfacao: number;
+  duplicado: boolean;
+  comentario: string;
+}
+
+const respostas: RespostaPesquisa[] = [
+    {
+    id: 1,
+    usuario: "Thais",
+    notaSatisfacao: 5,
+    duplicado: false,
+    comentario: "Amei o app!"
+}, {
+    id: 1,
+    usuario: "Thais",
+    notaSatisfacao: 5,
+    duplicado: false,
+    comentario: "Amei o app!"
+}, 
+  { id: 2, 
+    usuario:"Jessica",
+    notaSatisfacao: 2, 
+    duplicado: false, 
+    comentario: "Poderia ser mais rápido." },
+{
+    id: 3,
+    usuario: "Thais",
+    notaSatisfacao: 5,
+    duplicado: true,
+    comentario: "Amei o app!"
+}, 
+  { id: 4, 
+    usuario:"Ana bia",
+    notaSatisfacao: 4, 
+    duplicado: false, 
+    comentario: "Curti a navegação."},
+
+  { id: 5, 
+    usuario:"Carol",
+    notaSatisfacao: 1, 
+    duplicado: false, 
+    comentario: "Muitos bugs."},
+];
+
+//Crie uma função buscarUsuario que receba o array e um nome, e retorne a nota e comentário do usuário ou "Usuário não encontrado".
+
+function buscarUsuario(respostas: RespostaPesquisa[], usuario: string): void {
+
+}
